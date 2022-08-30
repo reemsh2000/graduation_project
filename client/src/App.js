54cd58b7cf8baeed1086373f7bdfe7bd8f6400f5
reemsh2000 <sh951652@gmail.com>
2022-08-30 09:34:42 +0300

    client inialization

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h2> Graduation project</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
