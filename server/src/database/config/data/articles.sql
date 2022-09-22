bc21eab04d272d16899a72733343385f17c3f5de
reemsh2000 <sh951652@gmail.com>
2022-09-22 10:38:29 +0300

    add articles data

---
+++ server/src/database/config/data/articles.sql
@@ -0,0 +1,31 @@
+INSERT INTO articles(title, content, image) 
+VALUES 
+  (
+    'How long does it take to lose weight?', 
+    'How long does it take to lose weight? In the age of instant gratification and dubious marketing claims, it may be difficult to have realistic expectations. Not to mention that when you feel uncomfortable in your own skin, and your health seems to be getting worse, you may be tempted to set yourself an ambitious weight loss goal. 
+     This is understandable. A big target number may make you feel more motivated and eager to start your transformation. But excessive weight loss can not only lead to the dreaded -yo-yo effect- (where you lose weight, only to put it back on), it can also be unhealthy. What is more, some people will find it way more difficult to shed unwanted pounds than others. So what is a realistic weight loss rate? And what factors can affect it?
+     Before we dive into the science of weight loss, it is important to point out that there is no real ‘ideal weight to aim for. Medical professionals often rely on a person’s BMI(opens in new tab) to determine what is deemed to be a ‘normal’ weight range. But this measurement is not without its flaws.', 
+    'https://cdn.mos.cms.futurecdn.net/LxSugXzC4p7FkpSoJXqSnN-970-80.jpg.webp'
+  ), 
+    (
+    'High-sugar diet disrupts the gut microbiome, leading to obesity', 
+    'Sugar may disrupt the community of bacteria living in the gut, thereby depleting crucial immune cells and causing obesity down the line, a new mouse study suggests. 
+     So far, the results have been shown only in mice. But if follow-up studies show similar trends in humans, that could eventually lead to treatments for metabolic disease and obesity, said senior author Ivaylo Ivanov, an associate professor of microbiology and immunology at Columbia University Vagelos College of Physicians and Surgeons. 
+     In the recent study, published online Aug. 29 in the journal Cell, scientists found that feeding mice a high-sugar diet containing sucrose and maltodextrin caused specific bacteria, called segmented filamentous bacteria SFB, in the mices intestines to die due to an overgrowth of different gut bugs. The sudden loss of SFB triggered a chain reaction in the mouse gut that ultimately changed how the animals absorbed dietary fat. ', 
+    'https://cdn.mos.cms.futurecdn.net/GtKQzPERHKxSESK849odaH-970-80.jpg.webp'
+  ), 
+    (
+    'Vitamin B12: Sources, benefits & deficiency', 
+    'Vitamin B12 is one of eight B vitamins needed for a range of important physiological functions. It’s found naturally in animal products and fortified foods, or can be taken via supplement form or administered via injection. 
+    Vitamin B12 is crucial to the human body, which needs it to produce new DNA, red blood cells, proteins, hormones and fats,” explains Heather Mangieri, a registered dietitian nutritionist. “Vitamin B12 is also key to the health of nerves. It’s part of the vitamin B complex, which includes thiamin, niacin, vitamin B6 and folate. Each of these vitamins has its own unique role in the body, but most B vitamins have a role in helping your bodys cells produce energy.”
+    The typical American diet meets or exceeds the daily recommended amount of vitamin B12 (2.4 micrograms). However, vitamin B12 deficiency remains a common problem in the United States because it can be affected by other factors, like age and digestion. Older adults, those on a vegan diet and pregnant women are especially prone to being deficient. ', 
+    'https://cdn.mos.cms.futurecdn.net/k2MRnuVELoJpemkRn3GQaD-970-80.jpg.webp'
+  ), 
+  (
+    'Is protein good for weight loss?', 
+    'While weight loss is not always the goal when it comes to leading a balanced lifestyle, making a few changes to your diet can be a helpful way to lose weight healthily. But instead of following a fad diet, why not follow the science? In which case, could simply eating more protein be good for weight loss? 
+     Protein is a macronutrient that plays a key role in our bodys health and normal function. Studies have found that it can also help to keep us fuller for longer, potentially playing a role in successful weight loss.
+     Here, we will discuss how protein can help with weight loss, where to get the best sources of protein from your diet, and other tips for losing weight in a sustainable way. In the meantime, be sure to check out our guide to the best protein bars. Like drinking your protein in shake form? We have got rounded up the best protein shaker too.
+', 
+    'https://cdn.mos.cms.futurecdn.net/UotSPknUsjwLKgGppbgNYV-970-80.jpg.webp'
+  );
