66e12c509efe6648be681f0a4ff25a806c353c55
reemsh2000 <sh951652@gmail.com>
2022-10-06 08:47:15 +0300

    put all queries in index file

--- server/src/database/queries/index.js
+++ server/src/database/queries/index.js
@@ -1,4 +1,11 @@
 const getArticlesQuery = require("./getArticlesQuery");
+const getActivitiesQuery = require("./getActivitiesQuery");
+const getNaturalIngradientsQuery = require("./getNaturalIngradientsQuery");
+const getHealthCareVideosQuery = require("./getHealthCareVideosQuery");
+
 module.exports = {
-    getArticlesQuery,
+  getArticlesQuery,
+  getActivitiesQuery,
+  getNaturalIngradientsQuery,
+  getHealthCareVideosQuery,
 };
