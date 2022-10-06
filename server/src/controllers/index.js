e6a6de6a3f5d5bf6f67a823a9a155070745eac2f
reemsh2000 <sh951652@gmail.com>
2022-10-06 08:58:13 +0300

    export anf import controllers function

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -1,6 +1,14 @@
-const getRecipe = require('./recipes/getRecipe');
-const getArticles = require('./articles/getArticles');
-module.exports={
-    getRecipe,
-    getArticles
-}
+const getRecipe = require("./recipes/getRecipe");
+const getArticles = require("./articles/getArticles");
+const getActivities = require("./activities/getActivities");
+const getNaturalIngradients = require("./naturalIngradients/getNaturalIngradients");
+const getHealthCareVideos = require("./healthCareVideos/getHealthCareVideos");
+
+module.exports = {
+  getRecipe,
+  getArticles,
+  getArticles,
+  getActivities,
+  getNaturalIngradients,
+  getHealthCareVideos,
+};
