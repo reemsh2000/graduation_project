cb057fae6fc5f18e96483b9b1e241d10093ec582
reemsh2000 <sh951652@gmail.com>
2022-10-06 08:59:34 +0300

    add new routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,18 @@
 const router = require("express").Router();
-const { getRecipe, getArticles } = require("../controllers");
+const {
+  getRecipe,
+  getArticles,
+  getActivities,
+  getHealthCareVideos,
+  getNaturalIngradients,
+} = require("../controllers");
 router.get("/", (req, res) => {
   res.json({ hi: "start" });
 });
 router.get("/recipes/:mealName", getRecipe);
 router.get("/articles", getArticles);
+router.get("/activities", getActivities);
+router.get("/healthCareVideos", getHealthCareVideos);
+router.get("/naturalIngradients", getNaturalIngradients);
+
 module.exports = router;
